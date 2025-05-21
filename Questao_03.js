c9d605deeef38e057d121c79d4c76c0be5db28a2
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-05-20 21:07:59 -0300

    Update Questao_03.js

--- Questao_03.js
+++ Questao_03.js
@@ -1,1 +1,28 @@
+let nome = "João da Silva";
+let nota = 7;
+let nota2 = 8.5;
+let notaf = nota * 0.6;
+let notaf2 = nota2 * 0.4;
+let notaFinal = notaf + notaf2;
+alert("Nome do aluno: " + nome + "\nNota 1: " + nota + "\nNota 2: " + nota2);
+alert("Nome do aluno: " + nome + "\nNota final: " + notaFinal);
 
+
+let nome = "Pedro";
+let nota = 6;
+let nota2 = 3;
+let notaf = nota * 0.6;
+let notaf2 = nota2 * 0.4;
+let notaFinal = notaf + notaf2;
+alert("Nome do aluno: " + nome + "\nNota 1: " + nota + "\nNota 2: " + nota2);
+alert("Nome do aluno: " + nome + "\nNota final: " + notaFinal);
+
+
+let nome = "Luana";
+let nota = 10;
+let nota2 = 5;
+let notaf = nota * 0.6;
+let notaf2 = nota2 * 0.4;
+let notaFinal = notaf + notaf2;
+alert("Nome do aluno: " + nome + "\nNota 1: " + nota + "\nNota 2: " + nota2);
+alert("Nome do aluno: " + nome + "\nNota final: " + notaFinal);
